0399d3525082f8fb5fb451ba590a18e1f284efca
Sibil Sarjam Soren <sibil.sorenofficial@gmail.com>
2024-10-15 22:22:24 +0530

    live url added

--- utils/socket.ts
+++ utils/socket.ts
@@ -1,5 +1,5 @@
 import { io, Socket } from "socket.io-client";
 
-const socket: Socket = io("http://localhost:5000"); // Backend URL
+const socket: Socket = io("https://cs2-veto-server.onrender.com"); // Backend URL
 
 export default socket;
